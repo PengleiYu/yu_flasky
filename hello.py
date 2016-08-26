9156fc5b6c6084e81337e6b283a8f9997a1c3cec
Yu Penglei <yupenglei@YudeMacBook-Pro.local>
2016-08-26 13:41:46 +0800

    第4章完成

--- hello.py
+++ hello.py
@@ -1,26 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-manager = Manager(app=app)
-bootstrap = Bootstrap(app=app)
-moment = Moment(app=app)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route('/')
-def index():
-    # user_agent = request.headers.get('User-Agent')
-    # return '<h1>Hello world!</h1>''<p>Your browser is %s</p>' % user_agent
-    return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    # return '<h1>Hello, %s!</h1>' % name
-    return render_template('user.html', name=name)
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -33,6 +29,20 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you changed your name!')
+        # name = form.name.data
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+
 if __name__ == '__main__':
-    # app.run(debug=True)
     manager.run()
