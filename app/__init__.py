3267431a628467fb1e04558c315f2f90fdbb9a39
Yu Penglei <yupenglei@YudeMacBook-Pro.local>
2016-08-31 10:36:53 +0800

    创建auth蓝本，auth路由，并在app工厂函数中附加auth蓝本

--- app/__init__.py
+++ app/__init__.py
@@ -26,5 +26,7 @@ def create_app(config_name):
     # todo:附加路由和自定义的错误页面
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
