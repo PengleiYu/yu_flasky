e48e1b2e1347cdbe58c2b5dd1374d74cd7e9122b
Yu Penglei <yupenglei@YudeMacBook-Pro.local>
2016-09-01 15:53:32 +0800

    用户头像：用户模型添加email哈希字段

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, abort, flash
-from flask.ext.login import login_required, current_user
+from flask_login import login_required, current_user
 from . import main
 from .forms import EditProfileForm, EditProfileAdminForm
 from .. import db
